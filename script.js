c9fd44558122454e0a383570c0149b75e520c733
Krzysiek Makówka <krzysiekmakowka9@gmail.com>
2021-06-14 00:58:04 +0200

    skills section for mobile

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    // variables for function which handle classes 
-    const helloWordElements = document.querySelectorAll("[data-helloWord='helloWordText'] > p");
-    const helloWordLetterArray = Array.from(helloWordElements);
-    // function which add class to element when it is under cursors 
+window.addEventListener('DOMContentLoaded', () => {
+    const helloWordLetterArray = Array.from(document.querySelectorAll("[data-helloText='helloText'] > p"));
+
+    // function which add class to element when it is under cursors
     addEventListener("mousemove", helloWordLetterArray.forEach(letter => {
         let letterUnderMouse = null;
         letter.addEventListener("mouseenter", () =>{
@@ -11,22 +10,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
         })
         letter.addEventListener("mouseout", () =>{
             letterUnderMouse.classList.remove("activeLetter");
+            letterUnderMouse = null;
         })
-        letterUnderMouse = null;
-    })
-    )
+    }))
+
     // variables for setInterval below 
     const hours = document.querySelector("[data-clock='hours']");
     const minutes = document.querySelector("[data-clock='min']");
     const seconds = document.querySelector("[data-clock='sec']");
+
     // setInterval which handle clock tips
     setInterval(() => {
         let day = new Date();
-        let hr = day.getHours() * 30 ;
+        let hr = day.getHours() * 30;
         let min = day.getMinutes() * 6;
         let sec = day.getSeconds() * 6;
         hours.style.transform = `rotateZ(${(hr)+(min/12)}deg)`;
         minutes.style.transform = `rotateZ(${(min)}deg)`;
         seconds.style.transform = `rotateZ(${(sec)}deg)`;
-    }, 1000);
+    }, 1000)
 });
